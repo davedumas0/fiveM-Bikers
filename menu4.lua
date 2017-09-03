1847e48d18d34a2e880205b727328c7408d649cb
davedumas0 <davedumas0@yahoo.com>
2017-09-03 14:23:00 -0700

    Update menu4.lua

--- menu4.lua
+++ menu4.lua
@@ -58,30 +58,167 @@ end
 ------------------------------------------------------------------
 --[[*************************************************************]]
 function Main()
-
+    interiorID = GetInteriorAtCoords(GetEntityCoords(GetPlayerPed(PlayerId()), true))
+	roomKey = GetRoomKeyFromEntity(GetPlayerPed(PlayerId()))
     markerLoop = false
     DisplayHelpText("Use ~INPUT_CELLPHONE_UP~ ~INPUT_CELLPHONE_DOWN~ to ~y~move~w~ and ~y~Enter~w~ to ~r~select")
 	Notify("Press ~r~F5 ~w~to ~g~open~w~/~r~close~w~!")
-    options.menu_title = "~r~TBA"
-    options.menu_subtitle = "~o~main menu"
+    options.menu_title = "~r~TESTING menu"
+    options.menu_subtitle = "~r~NATIVE TESTING ~b~menu 4"
     ClearMenu()
-    Menu.addButton("~r~TBA ~b~menu", "Main", nil)
+    Menu.addButton("~r~TEST 1 ~b~menu", "temp", nil)
+    Menu.addButton("~r~TEST 2 ~b~menu", "temp1", nil)
+    Menu.addButton("~r~TEST 3 ~b~menu", "temp2", nil)	
 end
 
-
+function temp()
+d = NetworkSessionChangeSlots(6, false)
+Notify(tostring(d))
+end
+function temp1()
+t = ParticipantId()--Citizen.InvokeNative(0x99B72C7ABDE5C910)
+Notify("~r~whats this")
+Notify(tostring(t))
+end
+function temp2()
+ for i = 0, 25 do
+  if DoesEntityExist(GetPlayerPed(i)) and not PlayerId() == i then
+    player2ID = i
+   if DoesEntityExist(GetPlayerPed(i)) and not PlayerId() == player2ID then
+    player3ID = i
+   end
+ end
+ end
+q = Citizen.InvokeNative(0x1153FA02A659051C)
+Notify("~y~whats this")
+Notify(tostring(q))
+Notify(tostring(PlayerId()))
+end
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --Press F5 to open/close menu
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(0)
-	markerTableLength = #markerTable	
-                    if IsControlJustReleased(1, 168) then -- INPUT_CELLPHONE_DOWN
-                        Main() -- Menu to draw
+        Citizen.Wait(0)	
+                    if IsControlJustReleased(1, 56) then -- INPUT_CELLPHONE_DOWN
+                       
+
+   					    Main() -- Menu to draw
                         Menu.hidden = not Menu.hidden -- Hide/Show the menu
                     end
                     Menu.renderGUI(options) -- Draw menu on each tick if Menu.hidden = false
                 end       
 end)
-
+--------------------------------------------------------------------------------------
+-------------------------------blips and markers--------------------------------------
+--------------------------------------------------------------------------------------
+Citizen.CreateThread(function()
+ local temp2 = AddBlipForCoord(-1471.9132568359, -920.27752685547, 9.1242156982422)
+    SetBlipSprite(temp2, 375)
+    SetBlipAsShortRange(temp2, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp2)
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, -1471.9132568359, -920.27752685547, 9.1242156982422, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false)
+	
+    end
+ end)
+Citizen.CreateThread(function()
+ local temp1 = AddBlipForCoord(-1147.5658203125, -1577.5626464844, 3.5033765792847)
+    SetBlipSprite(temp1, 375)
+    SetBlipAsShortRange(temp1, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp1)
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, -1147.5658203125, -1577.5626464844, 3.5033765792847, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )
+    end
+ end)
+Citizen.CreateThread(function()
+ local temp1 = AddBlipForCoord(1731.4437744141, 3707.8814453125, 33.00584487915)
+    SetBlipSprite(temp1, 375)
+    SetBlipAsShortRange(temp1, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp1)
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, 1731.4437744141, 3707.8814453125, 33.00584487915, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )
+    end
+ end)
+Citizen.CreateThread(function()
+ local temp1 = AddBlipForCoord(56.000817871094, -1045.69609375, 28.465732192993)
+    SetBlipSprite(temp1, 375)
+    SetBlipAsShortRange(temp1, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp1)
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, 56.000817871094, -1045.69609375, 28.465732192993, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )
+    end
+ end)
+Citizen.CreateThread(function()
+ local temp1 = AddBlipForCoord(-41.689205932617, 6417.0833007813, 30.590455627441)
+    SetBlipSprite(temp1, 375)
+    SetBlipAsShortRange(temp1, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp1)
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, -41.689205932617, 6417.0833007813, 30.590455627441, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )
+    end
+ end)
+Citizen.CreateThread(function()
+ local temp2 = AddBlipForCoord(2467.8368164063, 4100.8711914062, 36.864682006836)
+    SetBlipSprite(temp2, 375)
+    SetBlipAsShortRange(temp2, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp2)
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, 2467.8368164063, 4100.8711914062, 36.864682006836, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )
+    end
+ end)
+Citizen.CreateThread(function()
+ local temp2 = AddBlipForCoord(943.37563476563, -1458.60078125, 30.22310218811)
+    SetBlipSprite(temp2, 375)
+    SetBlipAsShortRange(temp2, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp2)
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, 943.37563476563, -1458.60078125, 30.22310218811, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )
+    end
+ end)
+Citizen.CreateThread(function()
+ local temp2 = AddBlipForCoord(178.11896972656, 308.55670166016, 104.47113952637)
+    SetBlipSprite(temp2, 375)
+    SetBlipAsShortRange(temp2, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp2) 
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, 178.11896972656, 308.55670166016, 104.47113952637, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )
+    end
+ end)
+Citizen.CreateThread(function()
+ local temp2 = AddBlipForCoord(-17.055588912964, -193.83917236328, 51.458997344971)
+    SetBlipSprite(temp2, 375)
+    SetBlipAsShortRange(temp2, false)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Biker Clubhouse")
+    EndTextCommandSetBlipName(temp2) 
+    while true do
+      Citizen.Wait(0)
+     DrawMarker(1, -17.055588912964, -193.83917236328, 51.458997344971, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 2.5, 200, 233, 0, 150, 0, 0, 2, 0, 0, 0, false )
+    end
+ end)
